89b59c700ccacb24fd38e60d4136e6cf0c88d436
amox-hub <abdoabdo12145254@gmail.com>
2026-02-07 01:40:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Amox AI", page_icon="⚡")
 st.title("⚡ Amox AI Chatbot")
 
 # مفتاح الـ API الخاص بك (موجود في صورتك)
-API_KEY = "AIzaSyAPQxFd26DrXkCbrNLxlUFwveJLr0tKhpQ" 
+API_KEY = "AIzaSyAPQxFd26DrXkCbrNLxlUFwveJLr0tKhpQ"
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel('gemini-pro')
 
